fb25237696bf42d7c609b2cb429217706267e48d
MD_Bolash <md_bolash@cs104u-4e.acad.fullerton.edu>
2019-08-31 14:45:33 -0700

    Meal Calculator

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,2 +1,21 @@
-// Name: 
+// Matthew Dao:
 // This program calculates the tax and tip on a restaurant bill.
+#include <iostream>
+
+int main()
+{
+  double total;
+  double cost;
+  // Get the food cost.
+  std::cout << "What is the meal's 'cost' ? ";
+  std::cin >> cost;
+  double tax = cost * 0.0775;
+  double tip = cost * 0.20;
+  // Calculate the cost of the meal.
+  total = cost+tax+tip;
+
+  // Display the cost of the meal with tax and tip.
+  std::cout << "The \"total\" of the bill with tax and tip included is " << total << "$.";
+
+  return 0;
+}
